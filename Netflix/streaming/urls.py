6e05bac3e03d6f47db75c872be74bb4d09cc4444
Jacobo24 <114859002+Jacobo24@users.noreply.github.com>
2024-12-10 17:38:47 +0100

    autentificacion

--- Netflix/streaming/urls.py
+++ Netflix/streaming/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.movie_list, name='movie_list'),
+    path('api/movies', views.MovieList.as_view(), name='api_movie_list'),
+    path('api/movies/json', views.MovieList.as_view(), name='api_movie_list_json'),
     path('tv-shows', views.tvshow_list, name='tvshow_list'),
+    path('api/tv-shows', views.TVShowList.as_view(), name='api_tvshow_list'),
+    path('api/tv-shows/json', views.TVShowList.as_view(), name='api_tvshow_list_json'),
 ]
--- Netflix/streaming/views.py
+++ Netflix/streaming/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import *
+from .utils import fetch_movies, fetch_tv_shows
 
 # Create your views here.
 
@@ -10,3 +15,29 @@ def movie_list(request):
 def tvshow_list(request):
     tv_shows = TVShow.objects.all()
     return render(request, 'streaming/tvshow_list.html', {'tv_shows': tv_shows})
+
+class MovieList(APIView):
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+
+class TVShowList(APIView):
+    def get(self, request):
+        tv_shows = TVShow.objects.all()
+        serializer = TVShowSerializer(tv_shows, many=True)
+        return Response(serializer.data)
+
+# Vista basada a través de JSON.
+def movie_list_json(request):
+    try:
+        data = fetch_movies()
+        return JsonResponse(data, safe=False)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+def tv_show_list_json(request):
+    try:
+        data = fetch_tv_shows()
+        return JsonResponse(data, safe=False)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
