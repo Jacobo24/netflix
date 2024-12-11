bb4be01c2f481e0fa4fdc76b76eac1a6a7e93c37
Jacobo24 <114859002+Jacobo24@users.noreply.github.com>
2024-12-11 18:12:32 +0100

    NI IDEA

--- Netflix/streaming/urls.py
+++ Netflix/streaming/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.movie_list, name='movie_list'),
     path('api/movies', views.MovieList.as_view(), name='api_movie_list'),
     path('api/movies/json', views.MovieList.as_view(), name='api_movie_list_json'),
-    path('tv-shows', views.tvshow_list, name='tvshow_list'),
-    path('api/tv-shows', views.TVShowList.as_view(), name='api_tvshow_list'),
-    path('api/tv-shows/json', views.TVShowList.as_view(), name='api_tvshow_list_json'),
+    path('api/tvshows', views.TVShowList.as_view(), name='api_tvshow_list'),
+    path('api/tvshows/json', views.tv_show_list_json, name='api_tvshow_list_json'),
+    path('tvshows', views.tvshow_list, name='tvshow_list'),
 ]
